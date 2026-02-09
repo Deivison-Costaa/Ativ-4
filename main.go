411cc559e212df89f274e2e3b38922e4db3b1239
Deivison <costadeivison007@gmail.com>
2026-02-09 14:47:43 -0300

    refactor main function to implement command-line flag parsing and output file handling

--- main.go
+++ main.go
@@ -1,18 +1,29 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Fprintln(os.Stderr, "Uso: ec1lex <arquivo>")
+	outputFile := flag.String("o", "", "Arquivo de saída (padrão: stdout)")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Uso: ec1 [-o arquivo.s] <arquivo.ec1>\n")
+		fmt.Fprintf(os.Stderr, "Compilador EC1 - Gera código assembly x86-64\n\n")
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
+	if flag.NArg() != 1 {
+		flag.Usage()
 		os.Exit(2)
 	}
 
-	path := os.Args[1]
+	path := flag.Arg(0)
 	var data []byte
 	var err error
 	if path == "-" {
@@ -25,16 +36,41 @@ func main() {
 		os.Exit(2)
 	}
 
-	lex := NewLexer(string(data))
-	for {
-		tok, err := lex.NextToken()
+	parser, err := NewParser(NewLexer(string(data)))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	ast, err := parser.ParseProgram()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	// Gera código assembly
+	cg := NewCodeGenerator()
+	code := cg.Generate(ast)
+
+	// Escreve a saída
+	if *outputFile != "" {
+		err = os.WriteFile(*outputFile, []byte(code), 0644)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err.Error())
-			os.Exit(1)
+			os.Exit(2)
 		}
-		if tok.Type == "" {
-			break
+	} else if path != "-" && !strings.HasSuffix(path, ".ec1") {
+		// Se não especificou -o e o arquivo não termina em .ec1, imprime em stdout
+		fmt.Print(code)
+	} else if path != "-" {
+		// Gera nome do arquivo .s baseado no arquivo de entrada
+		outPath := strings.TrimSuffix(path, filepath.Ext(path)) + ".s"
+		err = os.WriteFile(outPath, []byte(code), 0644)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(2)
 		}
-		fmt.Printf("<%s, \"%s\", %d>\n", tok.Type, tok.Lexeme, tok.Pos)
+	} else {
+		fmt.Print(code)
 	}
 }
